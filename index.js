8b89aaffc57e5e342781e9d27724e7f34fad3c4b
Neaz-mq <neazmorshed666@gmail.com>
2023-10-19 20:36:43 +0600

    update the product according to need

--- index.js
+++ index.js
@@ -75,12 +75,20 @@ async function run() {
 
     // user related apis
 
+    app.get('/user', async(req, res) =>{
+      const cursor = userCollection.find();
+      const users = await cursor.toArray();
+      res.send(users)
+     
+  })
     app.post('/user', async(req, res) =>{
       const user = req.body;
       console.log(user);
       const result = await userCollection.insertOne(user);
       res.send(result);
   })
+
+  
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
